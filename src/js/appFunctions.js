44189767bcbe8d0ade6cf6e4393867c57de4cea5
Lithium02 <nicolas.mendoza-@hotmail.com>
2023-09-24 17:07:59 -0300

    First Commit

---
+++ src/js/appFunctions.js
@@ -0,0 +1,198 @@
+const { ipcRenderer } = require('electron')
+const maxResBtn = document.getElementById('maxResBtn')
+//const { login } = require('./launch')
+const playbtn = document.getElementById("play-btn")
+const settingsbtn = document.getElementById("settings-btn")
+const stBackbtn = document.getElementById("stbackButton")
+const contentArea = document.getElementById("contentArea")
+const settingsArea = document.getElementById("settingsArea")
+const applyRambtn = document.getElementById("applyRam")
+
+const ipc = ipcRenderer
+
+const versionsDropdown = document.getElementById('dropdown-menu');
+
+var minRam, maxRam = "";
+
+const versions = [ 
+    "1.20.2",
+    "1.20.1",
+    "1.20",
+    "1.19.4",
+    "1.19.3",
+    "1.19.2",
+    "1.19.1",
+    "1.19",
+    "1.18.2",
+    "1.18.1",
+    "1.18",
+    "1.17.2",
+    "1.17.1",
+    "1.17",
+    "1.16.5",
+    "1.16.4",
+    "1.16.3",
+    "1.16.2",
+    "1.16.1",
+    "1.16",
+    "1.15.2",
+    "1.15.1",
+    "1.15",
+    "1.14.4",
+    "1.14.3",
+    "1.14.2",
+    "1.14.1",
+    "1.14",
+    "1.13.2",
+    "1.13.1",
+    "1.13",
+    "1.12.2",
+    "1.12.1",
+    "1.12",
+    "1.11.2",
+    "1.11.1",
+    "1.11",
+    "1.10.2",
+    "1.10.1",
+    "1.10",
+    "1.9.4",
+    "1.9.3",
+    "1.9.2",
+    "1.9.1",
+    "1.9",
+    "1.8.9",
+    "1.8.8",
+    "1.8.7",
+    "1.8.6",
+    "1.8.5",
+    "1.8.4",
+    "1.8.3",
+    "1.8.2",
+    "1.8.1",
+    "1.8",
+    "1.7.10",
+    "1.7.9",
+    "1.7.8",
+    "1.7.7",
+    "1.7.6",
+    "1.7.5",
+    "1.7.4",
+    "1.7.3",
+    "1.7.2",
+    "1.7.1",
+    "1.7",
+    "1.6.4",
+    "1.6.3",
+    "1.6.2",
+    "1.6.1",
+    "1.6",
+    "1.5.2",
+    "1.5.1",
+    "1.5",
+    "1.4.7",
+    "1.4.6",
+    "1.4.5",
+    "1.4.4",
+    "1.4.3",
+    "1.4.2"
+]
+
+document.getElementById('dropdown-button').textContent = "1.20.2";
+
+versions.forEach(item => {
+    const div = document.createElement('div');
+    div.textContent = /* "Release  " + */ item;
+    div.className = 'dropdownitem';
+    div.addEventListener('click', function() {
+        document.getElementById('dropdown-button').textContent = this.textContent;
+    });
+    versionsDropdown.appendChild(div);
+});
+
+//MINIZE APP FUNCTION//
+minimizeBtn.addEventListener('click', ()=> {
+    ipc.send('minimizeApp')
+})
+
+function changeMaxResBtn(isMaximizedApp){
+    if(isMaximizedApp){
+        maxResBtn.title = 'Restore'
+        maxResBtn.classList.remove('maximizeBtn')
+        maxResBtn.classList.add('restoreBtn')
+    }else {
+        maxResBtn.title = 'Maximize'
+        maxResBtn.classList.remove('restoreBtn')
+        maxResBtn.classList.add('maximizeBtn')
+    }
+}
+ipc.on('isMaximized', () => {changeMaxResBtn(true)})
+ipc.on('isRestored', () => {changeMaxResBtn(false)})
+//MAXIMIZE RESTORE APP FUNCTION//
+maxResBtn.addEventListener('click', ()=> {
+    ipc.send('maximizeRestoreApp')
+})
+
+//CLOSE APP FUNCTION//
+closeBtn.addEventListener('click', ()=> {
+    ipc.send('closeApp')
+})
+
+settingsbtn.addEventListener('click', () => {
+    document.getElementById('contentArea').style.display = 'none';
+    document.getElementById('settingsArea').style.display = 'block';
+})
+
+stBackbtn.addEventListener('click', () => {
+    document.getElementById('contentArea').style.display = 'block';
+    document.getElementById('settingsArea').style.display = 'none';
+})
+
+applyRambtn.addEventListener('click', () => {
+    minRam = document.getElementById('minRam')
+    maxRam = document.getElementById('maxRam')
+})
+
+
+playbtn.addEventListener("click",() => {
+    user = document.getElementById('username').value;
+    if(!user){
+        console.log("error")
+    }else{
+        version = document.getElementById('dropdown-button').textContent;
+        if(!minRam || !maxRam){
+            ipc.send('play', {user: user, version: version, minRam: "2G", maxRam: "4G",})
+            document.getElementById('play-txt').textContent = "Loading";
+        }else{
+            ipc.send('play', {user: user, version: version, minRam: minRam, maxRam: maxRam,})
+            document.getElementById('play-txt').textContent = "Loading";
+        }
+    }
+});
+
+ipc.on('loaded', () => {
+    document.getElementById('play-txt').textContent = "Play";
+})
+
+
+ipcRenderer.on('load-input-value', (event, inputValue) => {
+    // Load the input value into your input element
+    let inputElement = document.getElementById('username')
+    inputElement.value = inputValue
+  })
+
+ipcRenderer.on('load-version-value', (event, versionValue) => {
+    // Load the input value into your input element
+    let inputVersionElement = document.getElementById('dropdown-button')
+    inputVersionElement.textContent = versionValue
+  })
+
+  ipcRenderer.on('load-ram-values', (event, minRamValue, maxRamValue) => {
+    if (minRamValue == String && minRamValue != ''){
+        document.getElementById('minRam').value = minRamValue
+        minRam = minRamValue
+    }
+    if(maxRamValue == String && maxRamValue != ''){
+        document.getElementById('maxRam').value = maxRamValue
+        maxRam = maxRamValue
+    }
+   })
---
+++ src/js/main.js
@@ -0,0 +1,161 @@
+const { Client, Authenticator } = require("minecraft-launcher-core");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const keytar = require('keytar');
+const ipc = ipcMain
+const os = require("os");
+
+userDirectory = os.homedir();
+
+//MC LAUNCH
+
+const runMC = (token, version, minRam, maxRam) => {
+  const launcher = new Client();
+  let opts = {
+      clientPackage: null,
+      // Simply call this function to convert the msmc Minecraft object into a mclc authorization object
+      authorization: token,
+      root: userDirectory + "/AppData/Roaming/.minecraft",
+      version: {
+          number: version,
+          type: "release"
+      },
+      memory: {
+          max: maxRam,
+          min: minRam
+      }  
+  };
+  console.log("Starting!");
+  launcher.launch(opts);
+  
+  launcher.on('debug', (e) => console.log(e));
+  launcher.on('data', (e) => console.log(e));
+  launcher.on('progress', (e) => console.log(e));
+}
+
+const login = (user,version, minRam, maxRam) => {
+    Authenticator.getAuth(user).then(token => runMC(token, version, minRam, maxRam))
+  
+}
+
+
+
+
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = async () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 620,
+    minWidth: 1024, 
+    minHeight: 620,
+    maxWidth: 1024,
+    maxHeight: 620,
+    frame: false,
+    icon: __dirname + '../../build/icon.ico',
+    webPreferences: {
+      preload: path.join(__dirname, 'app.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+
+    },
+
+
+  });
+
+  let inputValue = await keytar.getPassword('bones-launcher', 'username')
+  let versionValue = await keytar.getPassword('bones-launcher', 'version')
+  let minRamValue = await keytar.getPassword('bones-launcher', 'minRam')
+  let maxRamValue = await keytar.getPassword('bones-launcher', 'maxRam')
+  console.log(inputValue)
+  console.log(versionValue)
+  // Send the loaded input value to renderer process
+  if(inputValue){
+    mainWindow.webContents.send('load-input-value', inputValue)
+  }
+  if(versionValue){
+    mainWindow.webContents.send('load-version-value', versionValue)
+  }
+  if (minRamValue && maxRamValue){
+    mainWindow.webContents.send('load-ram-values', {minRam: minRamValue, maxRam: maxRamValue})
+  }
+  
+
+  ipc.on('play', async (event, arg) =>{
+    await keytar.setPassword('bones-launcher', 'username', arg.user)
+    await keytar.setPassword('bones-launcher', 'version', arg.version)
+    await keytar.setPassword('bones-launcher', 'minRam', arg.minRam)
+    await keytar.setPassword('bones-launcher', 'maxRam', arg.maxRam)
+    login(arg.user, arg.version, arg.minRam, arg.maxRam)
+
+    event.sender.send('loaded');
+    console.log(arg.user)
+  })
+
+
+
+
+//APP TITLEBAR BUTTONS FUNCTIONS//
+
+  ipc.on('minimizeApp', () => {
+    mainWindow.minimize()
+  })
+
+  ipc.on('maximizeRestoreApp', () => {
+    if(mainWindow.isMaximized()){
+      mainWindow.restore()
+    } else {
+      mainWindow.maximize()
+    }
+  })
+
+  ipc.on('closeApp', () => {
+    mainWindow.close()
+  })
+
+  mainWindow.on('maximize', () => {
+    mainWindow.webContents.send('isMaximized')
+  })
+
+  mainWindow.on('unmaximize', () => {
+    mainWindow.webContents.send('isRestored')
+  })
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, '../public/index.html'));
+
+  //! Open DevTools. /DEBUG/
+  //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
